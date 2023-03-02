f9f6cfd9c40ed97559f6e8cf1fe6b2e38831ff07
xyf <xu_yanfeng@126.com>
2023-03-02 23:56:57 +0800

    init

--- assets/HelloShader.ts
+++ assets/HelloShader.ts
@@ -1,20 +1,10 @@
 const { ccclass, property } = cc._decorator;
 cc.dynamicAtlasManager.enabled = false;
 @ccclass
-export default class NewClass extends cc.Component {
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property(cc.Material)
-    material: cc.Material = null;
-
-
-
+export default class HelloShader extends cc.Component {
     start() {
         const render = this.getComponent(cc.Sprite);
         const material = render.getMaterial(0)
         material.setProperty('colorLeft', new cc.Vec4(0, 0, 1, 1));
     }
-
-    // update (dt) {}
 }
